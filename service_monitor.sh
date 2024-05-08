43aa39793cd2f8e4adf7bac08b262df724ac66d1
Hossein Alizadeh <hosseinalizadeh@gmail.com>
2024-05-08 14:59:21 +0330

    Add the Notification feature

--- service_monitor.sh
+++ service_monitor.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-# Path to the file to write warnings
-WARNING_FILE=./statuse_services.txt
-
-# Associative array to store previous statuses of services
+# Associative array for previous statuses of services
 declare -A prev_statuses
 
-# Function to check if a service status has changed and write warning if it has
+# Function to check service status and send a Rocket.Chat message
 check_status() {
-    local service_name=$1 
-    local current_status=$(systemctl is-active "$service_name" 2>/dev/null)
-    local prev_status=${prev_statuses["$service_name"]}
+  local service_name="$1"
+  local current_status=$(systemctl is-active "$service_name" 2>/dev/null)
+  local prev_status="${prev_statuses["$service_name"]}"
 
-    if [ -n "$current_status" ] && [ "$current_status" != "$prev_status" ]; then
-        if [ "$prev_status" != "unknown" ]; then
-            echo "$(date +"%Y/%m/%d %H:%M:%S")" - "$service_name service status changed from $prev_status to $current_status" >> "$WARNING_FILE"
-        fi
-        prev_statuses["$service_name"]="$current_status"
+  if [[ "$current_status" && "$current_status" != "$prev_status" && "$prev_status" != "unknown" ]]; then
+
+    # Prepare message payload with service status change details
+    TIME="$(date +"%Y/%m/%d %H:%M:%S")"
+    MESSAGE="$TIME - '"$service_name"' service changed from '"$prev_status"' to '"$current_status"'"
+
+    WEBHOOK="http://localhost:3000/hooks/663a1accbbcc52c0188b0096/X63YXmFKbsmC6EDESavANZrRdYKSaeLeM94ag75fyiowgK9K"
+
+    # Send message via curl (error suppressed)
+    curl -s -o /dev/null -X POST -H 'Content-Type: application/json' -d "{\"text\": \"${MESSAGE}\" }" ${WEBHOOK} 
     fi
+
+    prev_statuses["$service_name"]="$current_status"
+
 }
 
-# Initialize previous statuses for all services
+# Initialize previous statuses
 for service in $(systemctl list-units --type=service --no-pager --no-legend | cut -d '.' -f1); do
-##for service in $(systemctl list-units --type=service --no-pager --no-legend | awk '{print $1}'); do
-    prev_statuses["$service"]="unknown"
+  prev_statuses["$service"]="unknown"
 done
 
-# Loop indefinitely
+# Loop to check statuses
 while true; do
-    # Check status for each service
-    for service_name in "${!prev_statuses[@]}"; do
-        check_status "$service_name"
-    done
-
-    # Sleep for 3 second before next check
-    sleep 0
+  for service_name in "${!prev_statuses[@]}"; do
+    check_status "$service_name"
+  done
+  sleep 0
 done
